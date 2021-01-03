ae61fc04ce30b11f5d727357a880845fb0d46492
ivansoft <32499694+ivansoft@users.noreply.github.com>
2021-01-03 05:38:59 +0300

    restore openssl build configs from 2021-01

---
+++ SRC.native/openssl/shell.bat
@@ -0,0 +1,3 @@
+@call C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat
+@call C:\FX\LUA\SRC.external\strawberry-perl-5.32.0.1-64bit\portableshell.bat
+@set PATH=C:\FX\LUA\SRC.external\nasm-2.15.05;%PATH%
