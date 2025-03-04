70918bb8ee88bd02f004c911d8d20bf29496e94c
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-05 01:22:25 +0300

    update openssl shell

--- SRC.native/openssl/shell.bat
+++ SRC.native/openssl/shell.bat
@@ -1,3 +1,13 @@
-@call C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat
-@call C:\FX\LUA\SRC.external\strawberry-perl-5.32.0.1-64bit\portableshell.bat
-@set PATH=C:\FX\LUA\SRC.external\nasm-2.15.05;%PATH%
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+
+set PATH=%~dp0\nasm-2.16.03-bin;%PATH%
+call "%~dp0\strawberry-perl-5.40.0.1-bin\portableshell.bat" /SETENV
+
+perl -MConfig -e "print(qq{Perl executable: $^X\nPerl version   : $^V / $Config{archname}\n\n})" 2>nul
+if ERRORLEVEL==1 echo FATAL ERROR: 'perl' does not work; check if your strawberry pack is complete!
+
+:: https://github.com/openssl/openssl/blob/master/NOTES-WINDOWS.md
+pushd "%~dp0\openssl"
+	perl Configure VC-WIN64A
+	nmake
+popd
