78964aaca5add5018ab79bec5e47010fc2553fcf
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-07 21:37:02 +0300

    lua-5.4.1 update dumpbin64.bat

--- LUA/lua-5.4.1/dumpbin64.bat
+++ LUA/lua-5.4.1/dumpbin64.bat
@@ -3,23 +3,30 @@ setlocal
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-pushd "cmake-build-release-x64"
+rem set "CMAKE_BINARY_DIR=..\cmake-build-debug-x64"
+set "CMAKE_BINARY_DIR=..\cmake-build-release-x64"
 
-    dumpbin /headers lua.exe > lua.exe.headers.txt
-    dumpbin /exports lua.exe > lua.exe.exports.txt
-    dumpbin /dependents lua.exe > lua.exe.dependents.txt
-    dumpbin /imports lua.exe > lua.exe.imports.txt
+pushd %~dp0
+mkdir cmake-dump-release-x64
 
-    dumpbin /headers luac.exe > luac.exe.headers.txt
-    dumpbin /exports luac.exe > luac.exe.exports.txt
-    dumpbin /dependents luac.exe > luac.exe.dependents.txt
-    dumpbin /imports luac.exe > luac.exe.imports.txts
+pushd "cmake-dump-release-x64"
 
-    dumpbin /headers lua54.dll > lua54.dll.headers.txt
-    dumpbin /exports lua54.dll > lua54.dll.exports.txt
-    dumpbin /dependents lua54.dll > lua54.dll.dependents.txt
-    dumpbin /imports lua54.dll > lua54.dll.imports.txt
+    dumpbin /headers %CMAKE_BINARY_DIR%\lua.exe > lua.exe.headers.txt
+    dumpbin /exports %CMAKE_BINARY_DIR%\lua.exe > lua.exe.exports.txt
+    dumpbin /dependents %CMAKE_BINARY_DIR%\lua.exe > lua.exe.dependents.txt
+    dumpbin /imports %CMAKE_BINARY_DIR%\lua.exe > lua.exe.imports.txt
 
+    dumpbin /headers %CMAKE_BINARY_DIR%\luac.exe > luac.exe.headers.txt
+    dumpbin /exports %CMAKE_BINARY_DIR%\luac.exe > luac.exe.exports.txt
+    dumpbin /dependents %CMAKE_BINARY_DIR%\luac.exe > luac.exe.dependents.txt
+    dumpbin /imports %CMAKE_BINARY_DIR%\luac.exe > luac.exe.imports.txt
+
+    dumpbin /headers %CMAKE_BINARY_DIR%\lua54.dll > lua54.dll.headers.txt
+    dumpbin /exports %CMAKE_BINARY_DIR%\lua54.dll > lua54.dll.exports.txt
+    dumpbin /dependents %CMAKE_BINARY_DIR%\lua54.dll > lua54.dll.dependents.txt
+    dumpbin /imports %CMAKE_BINARY_DIR%\lua54.dll > lua54.dll.imports.txt
+
+popd
 popd
 
 endlocal
